076d2ad2488210b08218ff5f28e92a7674fd89b6
Alex Sushitskiy <magnetos.ua@gmail.com>
2020-12-20 00:09:14 +0200

    Add comments

--- config/ConfigDist.php
+++ config/ConfigDist.php
@@ -2,6 +2,9 @@
 
 namespace App\Config;
 
+/**
+ * Rename this file to Config.php and class to Config and set the real credentials to constants
+ */
 abstract class ConfigDist
 {
     const PROJECT_ID = 'test';
